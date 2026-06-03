b164522323b9ba0db39f3cbd90c04dc748106b47
Adscod1 <support@adscod.com>
2026-06-03 19:11:30 +0300

    Fix: use replace instead of replaceAll for RegExp in SITE_URL helper

--- app/src/lib/site.ts
+++ app/src/lib/site.ts
@@ -3,4 +3,4 @@
 // Set NEXT_PUBLIC_SITE_URL on the host to the real production domain.
 export const SITE_URL = (
   process.env.NEXT_PUBLIC_SITE_URL ?? "https://stressreliefclean.com"
-).replaceAll(/\/$/, "");
+).replace(/\/$/, "");
